323fec3911d3228eaf5889c9badd841eafe999fc
DenzelMbaki27 <bossmbaki1@gmail.com>
2026-02-20 19:08:30 -0500

    Add files via upload

---
+++ user.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  createUser,
+  getAllUsers,
+  getUserById
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+
+export default router;
